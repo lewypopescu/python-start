234337f86280f901397289ff16fb3f3ba54e19cc
lewypopescu <popesculewy@gmail.com>
2025-04-11 09:33:21 +0200

    first one :)

--- ZTM Exercices/input_test.py
+++
@@ -1,2 +0,0 @@
-name = input("What's your name? ")
-print(f"Nice to meet you, {name}!")
---
+++ ZTM Exercices/python1.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+print(f"Hellooo, Nice to meet you, {name}!")
+
+birth_year = input("What is your birthday year?")
+age = 2025 - int(birth_year)
+print(f"You are {age} years old, Happy Birthday to youuu!")
