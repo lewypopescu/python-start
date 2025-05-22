01e50311ec8f094f3ba14c1fa40b315f8df1c35a
lewypopescu <popesculewy@gmail.com>
2025-05-22 11:49:28 +0200

    class methos

--- ZTM Python3 Full 2025/main_03_OOP.py
+++ ZTM Python3 Full 2025/main_03_OOP.py
@@ -15,7 +15,7 @@ class PlayerCharacter:
 
     membership = True  # Class Object Attribute
 
-    def __init__(self, name, age):  # Constructor
+    def __init__(self, name, age):  # Constructor, # self - to refer to the instance of the class
         if (age > 18):
             self.name = name  # instance attribute
             self.age = age
@@ -26,6 +26,17 @@ class PlayerCharacter:
     def run(self, hello):
         print(f'Running {hello}...')
 
+# classmethod - to create a method that is bound to the class and not the instance of the class
+# staticmethod - to create a method that is bound to the class and not the instance of the class
+
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls("Teddy", num1 + num2)  # cls - to refer to the class itself
+
+    @staticmethod
+    def static_hello(hello):
+        print(f'Hello {hello}, here is a static method')
+
 
 player1 = PlayerCharacter("John", 25)
 player2 = PlayerCharacter("Jane", 22)
@@ -35,3 +46,6 @@ print(player1.name)
 print(player2.shout())
 print(player1.run("GoodYeyyy"))
 print(player2.power)
+
+player3 = PlayerCharacter.adding_things(10, 20)
+print(player3.name, player3.age)
